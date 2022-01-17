282f8872f5058121b0bf2fcfd34f093ce902717a
Leandro Vieira Rabelo da Silva <31897316+leandrovrabelo@users.noreply.github.com>
2022-01-17 00:59:20 -0300

    Update mov_detector.py

--- mov_detector.py
+++ mov_detector.py
@@ -6,24 +6,39 @@ import sys
 import numpy as np
 GPIO.setmode(GPIO.BOARD)
 
-curr_position = 0
-counter_speed = 0
-counter_infer_photo = 0
-counter_check_photo = 0   
+#curr_position = 0 # Starting position
+#counter_speed = 0 
+#counter_infer_photo = 0
+#counter_check_photo = 0   
 start = datetime.now()
-speed = 0
+#speed = 0
 
 def mov_detector(gpio_sensor=0, 
                 cm_hole=0,
-                disk_hole_qtt=0, 
-                height=0, 
-                infer_trigger=False, 
-                check_trigger=False, 
+                distance_interval=0, 
                 start_photo_inference=0, 
                 start_photo_check=0, 
                 spray_line1=0, 
                 spray_line2=0):
-                
+
+    '''
+    In this function we will use a disk with 20 or 40 holes and a radius of 5.1 cm,
+    the the end of the disk (perpendicular), we will use an infrared sensor to detect interruptions,
+    every interruption is a movement.
+
+    It will also be used to check the speed, trigger a photo for inference and checage.
+    Parameters:
+            gpio_sensor: this is the device port to connect as an input
+            cm_hole: it means how many centimeters each hole in the disk has (it's one step in the disk)
+            distance_interval: This is the distance in CM to be defined to trigger a photo
+            start_photo_inference: the minimal distance to trigger the photo for inference
+            start_photo_check: the minimal distance to trigger the check photo (check the spray)
+            spray_line1: if KeyBoard interrupt is triggered it turns off the solenoids in the first line
+            spray_line2: if KeyBoard interrupt is triggered it turns off the solenoids in the second line
+    
+    # TODO:
+            Use two sensors to detect the direction of the disk
+    '''
     # Setting initial time to measure the speed
     global curr_position
     global counter_speed
@@ -39,7 +54,7 @@ def mov_detector(gpio_sensor=0,
         if GPIO.wait_for_edge(gpio_sensor, GPIO.RISING):
             pass
 
-        if GPIO.wait_for_edge(gpio_sensor, GPIO.FALLING): # Change in signal
+        if GPIO.wait_for_edge(gpio_sensor, GPIO.FALLING): # Change in signal, counting steps
             curr_position += 1
             counter_speed += 1
             counter_infer_photo += 1
@@ -62,7 +77,7 @@ def mov_detector(gpio_sensor=0,
                 print('[WEEDS] Current speed: ', round(speed,2),' m/sec or: ', round(speed * 3.6, 2),' km/h.')
             # Triggering Inference Photo
             if curr_position >= start_photo_inference:
-                if counter_infer_photo * cm_hole >= height:
+                if counter_infer_photo * cm_hole >= distance_interval:
                     infer_trigger = True
                     counter_infer_photo = 0
                     print('[WEEDS] Inference Photo Triggered')
@@ -71,7 +86,7 @@ def mov_detector(gpio_sensor=0,
 
             # Triggering Check Photo
             if curr_position >= start_photo_check:
-                if counter_check_photo * cm_hole >= height:
+                if counter_check_photo * cm_hole >= distance_interval:
                     check_trigger = True
                     counter_check_photo = 0
                     print('[WEEDS] Check Photo Triggered')
